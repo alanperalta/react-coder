d0cd8b8130b9945a771f9b1890c2a46298276f67
Alan <alan.peralta@softdile.com>
2021-11-08 20:25:37 -0300

    Implementando typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App;
