3709b4ac116452856df61ff40d46f2ae439df3c8
Alan <alan.peralta@softdile.com>
2021-11-08 21:17:49 -0300

    NavBar inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <>
       <NavBar/>
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from '@mui/material/styles';
+
+export default function ButtonAppBar() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {isMobile? (
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          ) : null }
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            LA TIENDITA
+          </Typography>
+          <div>
+            <IconButton color="inherit">
+              Celulares
+            </IconButton>
+            <IconButton color="inherit">
+              Audio y video
+            </IconButton>
+            <IconButton color="inherit">
+              Gaming
+            </IconButton>
+            <IconButton color="inherit">
+              Calefacción
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
